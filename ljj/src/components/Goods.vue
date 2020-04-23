4031123d8f861b0a931ea2ba22e74ee553611f73
liujiajia <460686664@qq.com>
2020-04-23 18:16:39 +0800

    布局优化

--- ljj/src/components/Goods.vue
+++ ljj/src/components/Goods.vue
@@ -160,38 +160,38 @@ export default {
 </script>
 <style scoped>
     .firstStyle{
-        width: 408px;
-        height: 737px;
+        width: 100%;
+        height: 196vw;
     }
     .firstStyle .header{
         display: flex;
         align-items: center;
-        height: 67px;
-        padding-left: 24px;
-        border-top-right-radius: 25px;
-        border-top-left-radius: 25px;
+        height: 4.467rem;
+        padding-left: 1.6rem;
+        border-top-right-radius: 1.667rem;
+        border-top-left-radius: 1.667rem;
         background-color: #F5F5F5;
     }
     .firstStyle .header span{
-        font-size: 30px;
+        font-size: 2rem;
     }
     .firstStyle .header img{
-        width: 25px;
-        height: 25px;
-        margin-left: 5px;
+        width: 1.667rem;
+        height: 1.667rem;
+        margin-left: 0.333rem;
     }
     .firstStyle .banner{
         position: relative;
         width: 100%;
-        height: 408px;
+        height: 100vw;
     }
     .firstStyle .banner img {
-        width: 408px;
-        height: 408px;
+        width: 100%;
+        height: 100%;
     }
     .firstStyle .content{
         width: 100%;
-        margin-left: 24px;
+        margin-left: 1.6rem;
     }
     .firstStyle .banner div{
         position: absolute;
@@ -200,62 +200,65 @@ export default {
         display: flex;
         justify-content: center;
         align-items: center;
-        width: 207px;
-        height: 38px;
+        width: 13.8rem;
+        height: 2.533rem;
         color: white;
-        font-size: 27px;
+        font-size: 1.8rem;
         background-color: rgba(0,0,0,.5);        
     }
     .firstStyle .title{
-        margin-top: 24px;
-        font-size: 30px;
+        margin-top: 1.6rem;
+        font-size: 2rem;
     }
     .firstStyle .productStatus{
-        font-size: 24px;
+        font-size: 1.6rem;
+    }
+    .firstStyle .productStatus span{
+        font-size: 1.6rem;
     }
     .firstStyle .productPrice{
-        font-size: 22px;
+        font-size: 1.467rem;
     }
     .firstStyle .productPrice img{
-        width: 85px;
-        height: 30px;
-        margin-right: 10px;
+        width: 5.667rem;
+        height: 2rem;
+        margin-right: 0.667rem;
     }
     .firstStyle .productPrice span{
-        margin-left: 5px;
-        font-size: 36px;
+        margin-left: 0.333rem;
+        font-size: 2.4rem;
     }
     .firstStyle .footer{
         display: flex;
         align-items: center;
-        margin-top: 5px;
-        margin-left: 20px;
+        margin-left: 0.333rem;
+        margin-left: 1.333rem;
         list-style: none;      
     }
     .firstStyle .footer button{
-        height: 80px;
-        width: 180px;
-        font-size: 30px;
-        border-radius: 50px;
+        width: 11rem;
+        height: 5rem;
+        font-size: 1.8rem;
+        border-radius: 3.333rem;
     }
     .firstStyle .btn-buy{
-        margin-right: 10px;
+        margin-right: 0.667rem;
     }
 
 /* 第二张图布局 */
     .secondStyle{
-        width: 605px;
-        height: 360px;
+        width: 100%;
+        height: 59.5vw;
     }
     .secondStyle .left{
         position: relative;
         float: left;
-        width: 360px;
-        height: 360px;
+        width: 56vw;
+        height: 56vw;
     }
     .secondStyle .banner{
-        width: 360px;
-        height: 360px;
+        width: 100%;
+        height: 100%;
     }
     .secondStyle .shop{
         position: absolute;
@@ -264,28 +267,28 @@ export default {
         display: flex;
         justify-content: center;
         align-items: center;
-        width: 80px;
-        height: 30px;
-        margin-right: 12px;
-        margin-top: 12px;
-        border-radius: 20px;
+        width: 5.333rem;
+        height: 2rem;
+        margin-right: 0.8rem;
+        margin-top: 0.8rem;
+        border-radius: 1.333rem;
         color: white;
-        font-size: 14px;
+        font-size: 0.8rem;
         background-color: rgba(0,0,0,.6);
     }
     .secondStyle .shop img{
-        width: 25px;
-        height: 25px;
+        width: 1.667rem;
+        height: 1.667rem;
     }
     .secondStyle .right{
         position: relative;
         float: right;
-        width: 245px;
-        height: 360px;
+        width: 44vw;
+        height: 56vw;
     }
     .secondStyle .header{
-        padding-bottom: 10px;
-        margin-left: 12px;
+        padding-bottom: 0.3rem;
+        margin-left: 0.3rem;
         text-align: center;
         border-bottom:1px dashed #F0F0F0;
     }
@@ -296,8 +299,7 @@ export default {
         display: flex; 
         align-items: center; 
         justify-content: center;
-        margin-top: 15px;
-        font-size: 24px;
+        font-size: 1.2rem;
         font-weight: bold; 
     }
     .secondStyle .summary{
@@ -305,83 +307,86 @@ export default {
         justify-content: center;
         align-items: center;
         color: #9D9D9D;
-        font-size: 14px;
+        font-size: 0.8rem;
     }
     .secondStyle .productPrice{
-        /* display: flex;
-        align-items: center;
-        color: #EA4C33; */
-        font-size: 15px;
-        /* font-weight: bold; */
+        font-size: 0.8rem;
     }
     .secondStyle .productPrice img{
-        width: 60px;
-        height: 25px;
-        margin-right: 5px;
+        width: 3.3rem;
+        height: 1.2rem;
+        margin-right: 0.333rem;
     }
     .secondStyle .productPrice span{
-        margin-left: 5px;
-        font-size: 26px;
-        /* font-family: Arial, Helvetica, sans-serif; */
+        margin-left: 0.333rem;
+        font-size: 1.3rem;
     }
     .secondStyle .main{
-        position: absolute;
-        margin-top: 10px;
-        margin-left: 12px;
+        margin-left: 0.3rem;
     }
     .productMade{
-        margin-top: 8px;
+        margin-top: 0.533rem;
+        font-size: 0.9rem;
         font-weight: bold;
     }
     .secondStyle .productTitle{
         overflow: hidden; 
-        font-size: 15px;
+        white-space:nowrap;
+        text-overflow:ellipsis;
+        font-size: 0.8rem;
     }
-    .secondStyle .productStatus{ 
-        font-size: 15px;
+    .secondStyle .productStatus{
+        font-size: 0.8rem;
+    }
+    .secondStyle .productStatus span{
+        font-size: 0.8rem; 
     }
     .secondStyle .footer{
         position: absolute;
-        bottom: 10px;
-        margin-left: 12px;
-        font-size: 15px;
+        bottom: 0;
+        width: 100%;
+        margin-left: 0.3rem;
     }
     .secondStyle .tips{
+        font-size: 0.6rem;
         color: #9D9D9D;
     }
     .secondStyle .btn{
-        margin-top: 12px;
+        margin-top: 0.2rem;
         display: flex;
     }
     .secondStyle .btn button{
-        width: 109px;
-        height: 41px;
-        font-size: 14px;
-        border: 2px solid #EA4C33;
+        width: 5rem;
+        height: 2rem;
+        border: 0.1rem solid #EA4C33;
     }
     .secondStyle .btn img{
         display: flex;
         align-items: center;
-        width: 12px;
-        height: 12px;
+        width: 0.5rem;
+        height: 0.5rem;
+    }
+    .secondStyle .btn span{
+        font-size: 0.6rem;
+        white-space:nowrap;
+        overflow:hidden;
     }
     
     /* 第三张图布局*/
     .thirdStyle{
-        width: 678px;
-        height: 308px;
-        margin-top: 20px;
+        width: 100%;
+        height: 45vw;
     }
     .thirdStyle .left{
         position: relative;
         float: left;
-        width: 308px;
-        height: 308px;
+        width: 45vw;
+        height: 45vw;
         background-color: gray;
     }
     .thirdStyle .banner{
-        width: 308px;
-        height: 308px;
+        width: 100%;
+        height: 100%;
     }
     .thirdStyle .shop{
         position: absolute;
@@ -390,135 +395,141 @@ export default {
         display: flex;
         justify-content: center;
         align-items: center;
-        width: 103px;
-        height: 43px;
-        margin-right: 10px;
-        margin-top: 10px;
-        border-radius: 20px;
+        width: 5.5rem;
+        height: 2rem;
+        margin-right: 0.667rem;
+        margin-top: 0.667rem;
+        border-radius: 1.333rem;
         color: white;
-        font-size: 20px;
-        background-color: rgba(0,0,0,0.7);
-        
-        
+        background-color: rgba(0,0,0,.7);    
+    }
+    .thirdStyle .shop img{
+        width: 1.8rem;
+        height: 1.8rem;
     }
     .thirdStyle .right{
         position: relative;
         float: right;
-        width: 370px;
-        height: 308px;
+        width: 55vw;
+        height: 45vw;
     }
     .thirdStyle .header{
-        margin-left: 20px;
-        padding-bottom: 15px;
+        margin-left: 0.6rem;
+        padding-bottom: 0.5rem;
         border-bottom: 1px dashed #F0F0F0;
     }
-    .thirdStyle .header div{
-        height: 33%;
-    }
     .thirdStyle .productTitle{
-        margin-top: 24px;
-        font-size: 26px;
+        margin-top: 0.3rem;
     }
     .thirdStyle .productStatus{
-        font-size: 21px;
+        font-size: 0.8rem;
+    }
+    .thirdStyle .productStatus span{
+        font-size: 0.8rem;
     }
     .thirdStyle .productPrice{
-        /* display: flex;
-        align-items: center;
-        color: #EA4C33; */
-        font-size: 21px;
-        /* font-weight: bold; */
+        font-size: 0.8rem;
     }
     .thirdStyle .productPrice img{
-        widows: 70px;
-        height: 25px;
-        margin-right: 5px;
-        margin-top: 5px;
-        margin-bottom: 5px;
+        width: 2.6rem;
+        height: 1.2rem;
+        margin-right: 0.33rem;
+        margin-top: 0.33rem;
+        margin-bottom: 0.33rem;
     }
     .thirdStyle .productPrice span{ 
-        margin-left: 5px;
-        font-size: 30px;
-        /* font-family: Arial, Helvetica, sans-serif; */
+        margin-left: 0.33rem;
+        font-size: 1.2rem;
     }
     .thirdStyle .main{
-        margin-top: 10px;
-        margin-left: 20px;
+        margin-top: 0.3rem;
+        margin-left: 0.6rem;
         color: #9D9D9D; 
-        font-size: 22px;
+        font-size: 0.85rem;
+        overflow:hidden;
+        text-overflow:ellipsis;
+        display:-webkit-box;
+        -webkit-box-orient:vertical;
+        -webkit-line-clamp:2;
     }
     .thirdStyle .footer{
         position: absolute;
-        bottom: 10px;
+        bottom: 0.3rem;
         display: flex;
-        margin-left: 20px;
+        font-size: 1rem;
+        margin-left: 0.6rem;
     }
     .thirdStyle .footer button{
-        width: 160px;
-        height: 60px;
-        font-size: 22px;
-        border-radius: 30px;
+        width: 5.2rem;
+        height: 2rem;
+        font-size: 0.8rem;
+        border-radius: 1.667rem;
     }
     .thirdStyle .btn-buy{
-        margin-right: 10px;
+        margin-right: 0.6rem;
+    }
+    .thirdStyle .footer span{
+        white-space:nowrap;
+        overflow:hidden;
     }
     /* 第四张图的布局 */
     .forthStyle{
-        width: 330px;
-        height: 600px;
-        margin-top: 20px;
+        width: 100%;
+        height: 182vw;
+        margin-top: 1.33rem;
         background-color: #EA4433;
-        border-radius: 10px;
+        border-radius: 0.667rem;
     }
     .forthStyle .header{
         display: flex;
         align-items: center;
-        padding-top: 20px;
-        margin-left: 20px;
-        margin-right: 20px;
+        padding-top: 1.333rem;
+        margin-left: 1.333rem;
+        margin-right: 1.333rem;
         color: white;
-        font-size: 34px;
+        font-size: 2.8rem;
         font-weight: bold;
     }
     .forthStyle .header img{
-        width: 42px;
-        height: 42px;
+        width: 3.4rem;
+        height: 3.4rem;
     }
     .forthStyle .main{
-        margin: 8px 20px 28px 20px;
+        margin: 0.53rem 1.33rem 1.86rem 1.33rem;
         color: white;
-        font-size: 22px;
+        font-size: 1.85rem;
     }
     .forthStyle .footer{
-        margin-left: 20px;
-        margin-right: 20px;
+        margin-left: 1.33rem;
+        margin-right: 1.33rem;
     }
     .forthStyle .footer ul li {
-        width: 140px;
-        height: 182px;
-        margin-bottom: 10px; 
+        width: 10.8rem;
+        height: 10.8rem;
+        margin-bottom: 3rem;
     }
     .productBanner{
-        width: 140px;
-        height: 140px;
+        width: 10.8rem;
+        height: 10.8rem;
     }
     .productBanner img{
         height: 100%;
         width: 100%;
-        border-top-left-radius: 8px;
-        border-top-right-radius: 8px;
+        border-top-left-radius: 0.53rem;
+        border-top-right-radius: 0.53rem;
     }
     .forthStyle .productTitle{
         bottom: 0;
         display: flex;
         align-items: center;
-        height: 38px;
-        font-size: 20px;
+        height: 2.53rem;
+        font-size: 1rem;
         background-color: #F8F8F8;
-        border-bottom-left-radius: 8px;
-        border-bottom-right-radius: 8px;
+        border-bottom-left-radius: 0.53rem;
+        border-bottom-right-radius: 0.53rem;
     }
     .forthStyle .productTitle span{
-        margin-left: 10px;
+        font-size: 1.5rem;
+        margin-left: 0.6rem;
     }
 </style>
