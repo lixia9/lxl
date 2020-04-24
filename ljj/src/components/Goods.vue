ba824dce74824d46a77b0b3debae45f539c63620
lixiaolong <lixiaolong@PPD-0302000221deMacBook-Pro.local>
2020-04-24 14:34:47 +0800

    http get post

--- ljj/src/components/Goods.vue
+++ ljj/src/components/Goods.vue
@@ -1,9 +1,151 @@
 <template>
-   <div class="lxl">
-       <span>w</span>
-       <p>c</p>
-   </div>
-       
+    <div class="container">
+        <div class="firstStyle">
+            <div class="header">
+                <span>JL女装官方旗舰店</span>
+                <img src="../../static/images/arrow (2).png">
+            </div>
+            <div class="main">
+                <div class="banner">
+                    <img src="../../static/images/pic1.jpg">
+                    <div class="shop">想买人气28888</div>
+                </div>
+                <div class="content">
+                    <div class="title">春季亚麻条纹时尚上衣</div>
+                    <div class="productStatus">价格：
+                        <span>¥12345678</span>
+                    </div>
+                    <div class="productPrice">
+                        <img src="../../static/images/pic2.jpg">¥
+                        <span>6?6.00</span>
+                    </div>
+                </div>
+            </div>            
+            <div class="footer">
+                <button class="btn-buy">进店抢红包</button>
+                <button class="btn-basket">加购物车</button> 
+            </div>           
+        </div>
+
+        <div class="secondStyle">
+            <div class="left">
+                <img class="banner" src="../../static/images/pic1.jpg">
+                <div class="shop">
+                    <img src="../../static/images/point.png">
+                    直播中
+                </div>
+            </div>
+            <div class="right">
+                <div class="header">
+                    <div class="title">JNBY</div>
+                    <div class="summary">
+                        JL新潮国际旗舰店
+                        <img src="../../static/images/arrow.png">
+                    </div>
+                </div>
+                <div class="main">
+                    <div class="productMade">🇫🇷 法国</div>
+                    <div class="productTitle">春季亚麻条纹时尚性感衬衫</div>
+                    <div class="productPrice">
+                        <img src="../../static/images/pic2.jpg">¥
+                        <span>66?6.00</span>
+                    </div>
+                    <div class="productStatus">价格：
+                        <span>¥ 6666.00</span>
+                    </div>
+                </div>                
+                <div class="footer">
+                    <div class="tips">想买人气283838</div>
+                    <div class="btn">
+                        <button class="btn-buy">
+                            <span>进店抢红包</span>
+                            <img src="../../static/images/arrow (3).png">
+                        </button>
+                        <button class="btn-basket">
+                            <span>加购物车</span>
+                            <img src="../../static/images/arrow (4).png">
+                        </button>
+                    </div>          
+                </div>
+            </div>
+        </div>
+
+        <div class="thirdStyle">
+            <div class="left">
+                <img class="banner" src="../../static/images/pic1.jpg">
+                <div class="shop">
+                    <img src="../../static/images/point.png">
+                    直播中
+                </div>
+            </div>
+            <div class="right">
+                <div class="header">
+                   <div class="productTitle">春季亚麻条纹时尚性感上衣</div>
+                   <div class="productStatus">
+                       价格：
+                       <span>¥ 666.00</span>
+                    </div>
+                    <div class="productPrice">
+                        <img src="../../static/images/pic2.jpg">¥
+                        <span>6?6.00</span>
+                    </div>
+                </div>
+                <div class="main">
+                    布料质感很柔软，上身很舒服，极简主义，打造出青春气息
+                </div>
+                <div class="footer">
+                    <button class="btn-buy">进店抢红包</button>
+                    <button class="btn-basket">加购物车</button> 
+                </div>               
+            </div>
+        </div>
+
+        <div class="forthStyle">
+            <div class="header">
+                更多好宝贝
+                <img src="../../static/images/arrow-right.png">
+            </div>
+            <div class="main">
+                惊喜马上等你来揭晓
+            </div>
+            <div class="footer">
+                <ul>
+                    <li>
+                        <div class="productBanner">
+                            <img src="../../static/images/good1.jpg">
+                        </div>
+                        <div class="productTitle">
+                            <span>口味极佳</span>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="productBanner">
+                            <img src="../../static/images/good2.jpg">
+                        </div>
+                        <div class="productTitle">
+                            <span>肉多汁少</span>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="productBanner">
+                            <img src="../../static/images/good3.jpg">
+                        </div>
+                        <div class="productTitle">
+                            <span>美味到想哭</span>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="productBanner">
+                            <img src="../../static/images/good4.jpg">
+                        </div>
+                        <div class="productTitle">
+                            <span>富含营养</span>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>        
+    </div>
 </template>
 
 <script>
@@ -409,9 +551,5 @@ export default {
     .forthStyle .productTitle span{
         font-size: 1.5rem;
         margin-left: 0.6rem;
-    }
-
-    .lxl{
-        font-size: 20rem;
     }
 </style>
